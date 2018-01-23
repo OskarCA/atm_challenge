b48636fcfc6d7bbcf3a433ecc674df20d8c104b0
Isabelle <isabellelidberg@live.se>
2018-01-23 15:00:43 +0100

    total code makeover

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,22 +1,29 @@
 class Atm
-  attr_accessor :funds, :status
+  attr_accessor :funds
     #
     def initialize
         @funds = 1000
     end
 
-    def account
-        @balance = 100
-    end
     # NEW
     def withdraw(amount, account)
         case
-        when amount > account.balance
+        when insufficient_funds_in_account?(amount, account)
             return
         else
-            @funds -= amount
-            account.balance = account.balance - amount
-            { status: true, message: 'success', date: Date.today, amount: amount }
-    end
+          perform_transaction(amount, account)
+        end
+      end
+
+      private
+
+      def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
+      end
+
+      def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount:amount }
+      end
   end
-end
