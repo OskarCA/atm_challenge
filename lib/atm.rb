e3af1dce45660f21dd88d28beb5b5c3a20c35300
Oskar Brännström <oskar@quad.se>
2018-01-23 13:57:44 +0100

    Added instance_double

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,11 +1,16 @@
 class Atm
-  attr_accessor :funds
-    #Comment
+  attr_accessor :funds, :status
+    #
     def initialize
         @funds = 1000
     end
+
     def withdraw(amount)
         @funds -= amount
     end
 
+    def account
+        @balance = 100
+    end
+    
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,9 +1,20 @@
 require './lib/atm.rb'
 
 describe Atm do
+    let(:account) { instance_double('Account') }
+    # New line
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
+    # New line
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
+
+    # New line
     it 'funds are reduced at withdraw' do
         subject.withdraw 50
         expect(subject.funds).to eq 950
