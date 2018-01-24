66c72347704472e38dacb4b0bb916d13d0d776f9
Isabelle <isabellelidberg@live.se>
2018-01-24 14:27:53 +0100

    expire date years

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,6 @@
 class Account
   attr_accessor :pin_code, :balance, :account_status, :exp_date
 
+  STANDARD_VALIDITY_YRS = 5
+  
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,13 +2,24 @@ require './lib/account.rb'
 require 'date'
 
 describe Account do
+
     it 'checks length of a number' do
         number = random_pin
         number_length = Math.log10(number).to_i + 1
         expect(number_length).to eq 4
     end
+
+    it 'is expected to have an expiry date initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+    end
+
 end
 # Comment
-def random_pin
+  def random_pin
     rand(1000..9999)
-end
+  end
+
+  def set_expire_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+  end
