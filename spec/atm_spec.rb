de59bc295e0accf28de1e77ca349223210bd8f45
Isabelle <isabellelidberg@live.se>
2018-01-26 10:18:44 +0100

    still testing

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require 'atm.rb'
 require  'date'
 
 describe Atm do
-  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/18', account_status: active, :set_owner ) }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/18', account_status: :active, :set_owner) }
 
   before do
    allow(account).to receive(:balance).and_return(100)
@@ -44,6 +44,3 @@ describe Atm do
    expected_output = { status: false, message: 'card expired', date: Date.today }
    expect(subject.withdraw(6, '1234', account)).to eq expected_output
   end
-
-end
-end
